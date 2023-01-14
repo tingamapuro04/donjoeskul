da8a3e3da97c4b3057260b3f1e198e5ce6508c7d
tingamapuro04 <alponcemobutu100@gmail.com>
2023-01-14 16:18:28 +0300

    po

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.scss';
 import AdmitStudent from './components/AdmitStudent';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <AdmitStudent />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<AdmitStudent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
