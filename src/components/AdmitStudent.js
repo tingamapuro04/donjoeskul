264a707db63e150100504804d27b29308e7cddf7
tingamapuro04 <alponcemobutu100@gmail.com>
2023-01-14 10:31:42 +0300

    Add formik module

---
+++ src/components/AdmitStudent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useDispatch } from "react-redux";
+
+const AdmitStudent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdmitStudent
--- src/store/Student/studentSlice.js
+++ src/store/Student/studentSlice.js
@@ -1,8 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  students: [],
-};
+const initialState = []
 
 const studentSlice = createSlice({
   name: 'Students',
