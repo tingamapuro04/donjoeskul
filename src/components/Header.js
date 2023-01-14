e48cadcc49edcf27001b2a160bc45a529e3bd08b
tingamapuro04 <alponcemobutu100@gmail.com>
2023-01-14 17:41:59 +0300

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './style.scss'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./style.scss";
 
 const Header = () => {
+  const stylelink = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "700" : "400",
+      color: isActive ? "#909090" : "#369cff",
+      fontSize: isActive ? "1.6rem" : "1rem",
+    };
+  };
+
   return (
-    <nav>
+    <nav className="Navlink">
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink style={stylelink} to="/">
+            Home
+          </NavLink>
         </li>
 
         <li>
-          <NavLink to="/register">Register</NavLink>
+          <NavLink style={stylelink} to="/register">
+            Register
+          </NavLink>
         </li>
 
         <li>
-          <NavLink to="/students">Students</NavLink>
+          <NavLink style={stylelink} to="/students">
+            Students
+          </NavLink>
         </li>
       </ul>
     </nav>
   );
-}
+};
 
-export default Header
+export default Header;
