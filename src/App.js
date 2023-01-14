a1598548865937771322317a2672dfed4d7e0eec
tingamapuro04 <alponcemobutu100@gmail.com>
2023-01-14 16:31:19 +0300

    was

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.scss';
 import AdmitStudent from './components/AdmitStudent';
 import Home from './components/Home';
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<AdmitStudent />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header
